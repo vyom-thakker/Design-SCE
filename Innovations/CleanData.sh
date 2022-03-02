0c5188c6d4690cce8d69b84de2e0950d78313a5b
thakker.8 <thakker.8@cbe-dnc244508d.coeit.osu.edu>
2022-03-02 11:55:43 -0500

    now done

---
+++ Innovations/CleanData.sh
@@ -0,0 +1,7 @@
+file=$1
+filename=$file
+#filename="pareto"
+#filename="$filename$file"
+sed -i -e 's/\s\+//g' $filename
+#sed -i '1i Cost,DoC,TotPlastics(kg),HDPE,LDPE,PP,PLA,PHA,Paper,Reprocess,Pyrolysis,Landfill,Incineration,GWP' $filename
+sed -i '1i Cost,DoC,GWP,TotPlastics(kg),HDPE,LDPE,PP,PLA,PHA,Paper,Reprocess,Pyrolysis,Landfill,Incineration,LABS from chemical recycling of PE,C4 Gas Mixture Pyrolysis,Light Liquid Fuel Pyrolysis,Clay reycled PLA,Lipase based PLA recycling,Lactic acid from acid hydrolysis,Me-Lactate from alcoholysis,Clinker,Lumber' $filename
